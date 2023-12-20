ff19b4cfe4118e9d1b10cb8286f93e73479faab6
EK Nayeem <saeidex.dev@gmail.com>
2023-12-21 05:49:23 +0600

    Feature/layout (#1)
    * init: astro+tailwind
    
    * docs: fix typo
    
    * chore: add prettier format script
    
    * style: format files
    
    * refactor: change nav, color
    
    * fix: nav shrink
    
    * refactor: bg color swap
    
    * fix: main logo
    
    * chore: add bootstrap icons
    
    * refactor: change logo color
    
    * style: :truck: rename layout -> layouts
    
    * feat: remove source link
    
    * feat: add sidebar
    
    * refactor: add logo on sidebar
    
    * refactor: change color scheme
    
    * refactor: sidebar
    
    * chore: add Temp.astro
    
    * feat: add animated logo scrolling on nav
    
    * style: change sidebar card style
    
    * style: change nav style
    
    * feat: add hero placeholder

---
+++ tailwind.config.mjs
@@ -0,0 +1,36 @@
+/** @type {import('tailwindcss').Config} */
+const defaultTheme = require('tailwindcss/defaultTheme')
+
+export default {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		extend: {
+			// gridTemplateColumns: {
+			// 	layout: '300px minmax(600px, 1fr) 200px'
+			// },
+			fontFamily: {
+				'sans-serif': ['Space Mono', 'sans-serif'],
+				logo: ['Major Mono Display', 'sans-serif']
+			},
+			colors: {
+				'regal-blue': '#243c5a',
+				'clr-bg-primary': '#10151D',
+				'clr-bg-secondary': '#161E29',
+				'clr-border-primary': '#161e29',
+				'clr-border-secondary': '#2E3C51',
+				'clr-text-primary': 'rgb(191 199 210)', // text-gray-500
+				'clr-text-secondary': 'rgb(128 140 156)'
+			},
+			animation: {
+				'running-text': 'running-text 700s linear infinite alternate'
+			},
+			keyframes: {
+				'running-text': {
+					from: { transform: 'translateX(-90%)' },
+					to: { transform: 'translateX(-10%)' }
+				}
+			}
+		}
+	},
+	plugins: []
+}
